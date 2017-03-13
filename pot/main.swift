b1cd37549a986ecb13a019673a9e2b75f5123051
drhaynes <drhmbp@gmail.com>
2017-03-13 12:51:39 +0000

    Add resizing and command usage description

--- pot/main.swift
+++ pot/main.swift
@@ -6,10 +6,16 @@
 //
 //  Tool to resize images to the nearest power of two.
 //
+//  Usage: run in a folder to automatically rescale all .jpg files down to their
+//  nearest power of two.
+//
+//  Specify '-up' as an argument to scale up instead.
+//
+//  Note: Requires ImageMagick to be installed and available at path: /usr/local/bin
+//
 //  Created by David Haynes on 12/03/2017.
 //  Copyright © 2017 MBP Consulting Ltd. All rights reserved.
 //
-
 
 import Foundation
 
@@ -65,15 +71,62 @@ func getDimension(imagePath: String, dimension: Dimension, direction: ScaleDirec
     return 0
 }
 
+func resizeImage(imagePath: String, width: Int, height: Int) {
+    let widthString = String(width)
+    let heightString = String(height)
+
+    let convertPath = "/usr/local/bin/convert"
+    var resizeArguments = "{w}x{h}!"
+    resizeArguments = resizeArguments.replacingOccurrences(of: "{w}", with: widthString)
+    resizeArguments = resizeArguments.replacingOccurrences(of: "{h}", with: heightString)
+
+    let fileExtension = (imagePath as NSString).pathExtension
+    let fileName = (imagePath as NSString).deletingPathExtension
+    let outputImagePath = String(format: "%@-%dx%d.%@", fileName, width, height, fileExtension)
+
+    let task = Process()
+    task.launchPath = convertPath
+    task.arguments = [imagePath, "-resize", resizeArguments, outputImagePath]
+    task.launch()
+
+    print("Wrote:", outputImagePath)
+}
+
+func createDir(dirName: String) {
+    if !(FileManager.default.fileExists(atPath: dirName)) {
+        do {
+            try FileManager.default.createDirectory(atPath: dirName,
+                                                    withIntermediateDirectories: false,
+                                                    attributes: nil)
+        } catch let error as NSError {
+            print("Error creating directory: \(error.localizedDescription)")
+        }
+    }
+}
+
+func listAllFiles(atPath path: String, withExtension fileExtension: String) -> [String] {
+    let pathURL = URL(fileURLWithPath: path, isDirectory: true)
+
+    print("Searching for images in:", pathURL.absoluteString)
+
+    do {
+        let directoryContents = try FileManager.default.contentsOfDirectory(at: pathURL, includingPropertiesForKeys: nil, options: [])
+        let jpgFiles = directoryContents.filter{ $0.pathExtension == "jpg" }.map{ $0.relativePath }
+        return jpgFiles
+    } catch let error as NSError {
+        print(error.localizedDescription)
+    }
+    return []
+}
 
 let arguments: [String] = CommandLine.arguments.count <= 1 ? [] : Array(CommandLine.arguments.dropFirst())
 
-if let path = arguments.first {
-    let width = getDimension(imagePath: path, dimension: .width, direction: .up)
-    let height = getDimension(imagePath: path, dimension: .height, direction: .up)
-    print(width)
-    print(height)
-} else {
-    print("Usage: pot [path to image]")
+createDir(dirName: "resized")
+let files = listAllFiles(atPath: ".", withExtension: ".jpg")
+for filePath in files {
+    var direction = ScaleDirection.down
+    if (arguments.contains("-up")) { direction = .up }
+    let width = getDimension(imagePath: filePath, dimension: .width, direction: direction)
+    let height = getDimension(imagePath: filePath, dimension: .height, direction: direction)
+    resizeImage(imagePath: filePath, width: width, height: height)
 }
-
